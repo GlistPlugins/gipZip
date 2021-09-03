946ae782e3b9b4433eb27dd15f0bef9daed803e4
yavuzbilginoglu <yavuzbilginoglu@gmail.com>
2021-09-03 15:51:10 +0300

    gipZip
    fixed libs errors

---
+++ src/gipZip.h
@@ -0,0 +1,40 @@
+/*
+ * gipZip.h
+ *
+ *  Created on: 19 Aðu 2021
+ *      Author: yavuzbilginoglu
+ */
+
+#ifndef SRC_GIPZIP_H_
+#define SRC_GIPZIP_H_
+
+#include "gBasePlugin.h"
+#include "zip.h"
+#include "unzip.h"
+
+
+class gipZip : public gBasePlugin{
+public:
+
+	gipZip();
+	virtual ~gipZip();
+
+	bool open(std::string fullPath);
+	bool openzip(std::string zipPath);
+	bool closeZip(gipZip zip);
+
+	bool addFile(gipZip zip, std::string filePath);
+	bool closeFile(gipZip zipFile);
+
+//	std::vector<std::string> getFileList();
+
+
+private:
+	std::string fullpath, directory, filepath, extractpath,zippath;
+	std::string getDirName(const std::string& fullPath);
+	std::string getFileName(const std::string& fullPath);
+	zipFile zip;
+	zipFile zipFile;
+	unzFile unzip;
+};
+#endif /* SRC_GIPEMPTY_H_ */
